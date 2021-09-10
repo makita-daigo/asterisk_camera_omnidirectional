b45936ce8f3b408c3e49811abd1be6e90ad0ba85
makita <mahjong.dai3gen@gmail.com>
2021-09-10 16:20:34 +0900

    [add]パラメータ設定を追加

--- src/pov_camera_angle_rotation.cpp
+++ src/pov_camera_angle_rotation.cpp
@@ -8,9 +8,20 @@
 class BroadCasterTest
 {
 public:
+
+  std::string param_pov_camera_frame_;
+  std::string param_camera_frame_;
+  double loop_time_;
+  int direction_number_;
+
   BroadCasterTest() : nh_()
   {
-    timer_ = nh_.createTimer(ros::Duration(0.1), [&](const ros::TimerEvent& e) {
+    nh_.param("pov_camera_frame", param_pov_camera_frame_, std::string("flipped_pov_camera")); //カメラの視点方向のtf（子フレーム）
+    nh_.param("camera_frame", param_camera_frame_, std::string("camera/flipped_front"));       //カメラの位置のtf（親フレーム）
+    nh_.param("loop_time", loop_time_, 0.1);                                                   //tf更新する時間間隔[s]
+    nh_.param("direction_number", direction_number_, 6);                                       //視点の方向数
+
+    timer_ = nh_.createTimer(ros::Duration(loop_time_), [&](const ros::TimerEvent& e) {
       broadcast_dynamic_tf();
       counter_++;
     });
@@ -21,13 +32,13 @@ private:
   {
     geometry_msgs::TransformStamped transformStamped;
     transformStamped.header.stamp = ros::Time::now();
-    transformStamped.header.frame_id = "camera/flipped_front";
-    transformStamped.child_frame_id = "flipped_pov_camera";
+    transformStamped.header.frame_id = param_camera_frame_;
+    transformStamped.child_frame_id = param_pov_camera_frame_;
     transformStamped.transform.translation.x = 0;
     transformStamped.transform.translation.y = 0;
     transformStamped.transform.translation.z = 0;
     tf2::Quaternion q;
-    q.setRPY(0, 0, counter_ * M_PI / 6);
+    q.setRPY(0, 0, counter_ * M_PI / direction_number_);
     transformStamped.transform.rotation.x = q.x();
     transformStamped.transform.rotation.y = q.y();
     transformStamped.transform.rotation.z = q.z();
